68abfe4e7dadf381470ad55789eb2785f3405be3
Deepak <deepakprasad9554.com>
2024-07-07 20:27:33 +0530

    checking integer or not

---
+++ chapter 1/Java_03_isinteger.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Java_03_isinteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        System.out.println(sc.hasNextInt());
+
+    }
+}

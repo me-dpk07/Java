1ef331a7e3dd6c314218eae6b9ca99019685aaa1
Deepak <deepakprasad9554.com>
2024-07-08 13:09:06 +0530

    calculated percentage

--- chapter 1/Java_02_calculatingPercentage.java
+++ chapter 1/Java_02_calculatingPercentage.java
@@ -13,9 +13,12 @@ public class Java_02_calculatingPercentage {
         int d = sc.nextInt();
         System.out.println("Enter your Hindi marks");
         int e = sc.nextInt();
-        int sum = a + b +c + d + e;
-        int percent =  sum/5;
+        int Total = a + b +c + d + e;
+        float percent =  Total*100 /500;
         System.out.println("Your percentage is");
         System.out.println(percent);
+        float gpa = (percent*100)/4;
+        System.out.println("your gpa is");
+        System.out.println(gpa);
     }
 }

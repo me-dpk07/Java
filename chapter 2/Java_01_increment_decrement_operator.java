e68a54129b5e0de2aa0765ab4c5e9c1ba56eedda
Deepak <deepakprasad9554.com>
2024-07-08 12:50:47 +0530

    increment decrement operator

---
+++ chapter 2/Java_01_increment_decrement_operator.java
@@ -0,0 +1,9 @@
+package chapter 2;
+
+public class Java_01_increment_decrement_operator {
+    public static void main(String[] args) {
+        int y = 7;
+        int x = ++y*8; // first y will be incremented and then multiply
+        System.out.println(x);
+    }
+}

7d4c1bd0ab63aa59e25326237feb8f0ec94da28f
Deepak <deepakprasad9554.com>
2024-07-08 14:31:28 +0530

    comparison test that given number is greater or not

---
+++ chapter 2/Java_04_comparisontest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Java_04_comparisontest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number");
+        int a = sc.nextInt(); 
+        System.out.println(a>=8);
+    }
+    
+}

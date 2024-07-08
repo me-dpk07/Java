50067515bb17a56d5b8157587d6fd19aa630dbdb
Deepak <deepakprasad9554.com>
2024-07-08 13:08:24 +0530

    arthmetic expression

---
+++ chapter 2/Java_02_arthmetic_expression.java
@@ -0,0 +1,10 @@
+
+
+public class Java_02_arthmetic_expression {
+    public static void main(String[] args) {
+    // float a = 7/4*9/2;  ---> will return 4 because this will treat as integer
+        float a= 7/4.0f*9/2.0f;
+        System.out.println(a);
+    }
+    
+}

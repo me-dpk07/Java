91b2fe11a586c29e80204ab3e68cf62c945d7986
Deepak <deepakprasad9554.com>
2024-07-08 14:19:57 +0530

    encryption & decryption of grade

---
+++ chapter 2/Java_03_gradeEncryption.java
@@ -0,0 +1,14 @@
+
+public class Java_03_gradeEncryption {
+    public static void main(String[] args) {
+    char grade = 'B';
+    grade = (char) (grade+8);
+    System.out.println(grade);
+
+
+    // decrypting
+
+    grade = (char)(grade-8);
+        System.out.println(grade);
+}
+} 

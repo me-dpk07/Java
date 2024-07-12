6c7ce551bc99fb0f787b5596396bddf9b62f1d7a
Deepak <deepakprasad9554.com>
2024-07-12 09:05:37 +0530

    converting string into upper/lower case

---
+++ chapter 3/toConvertIntoUperCase.java
@@ -0,0 +1,7 @@
+public class toConvertIntoUperCase {
+    public static void main(String[] args) {
+       String name = "Deepak";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+   } 
+} 

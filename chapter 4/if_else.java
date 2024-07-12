ed1a5568af6e3697e40415659879d58f70c2b476
Deepak <deepakprasad9554.com>
2024-07-12 17:07:58 +0530

    solving if else problem

---
+++ chapter 4/if_else.java
@@ -0,0 +1,14 @@
+//Q---> 1
+
+public class if_else {
+    public static void main(String[] args) {
+        int a = 11;
+        if(a==11){
+            System.out.println("I am 11");}
+        else {
+        System.out.println("I am not 11");
+            }
+        }
+        
+    }
+ 

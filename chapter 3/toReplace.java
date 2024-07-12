cac5a24830be7b092c37680dfbd6843210c7dc06
Deepak <deepakprasad9554.com>
2024-07-12 09:14:51 +0530

    replacing white spaces to underscore

---
+++ chapter 3/toReplace.java
@@ -0,0 +1,10 @@
+public class toReplace {
+    public static void main(String[] args) {
+       String name = "Deepak prasad";
+        System.out.println(name.replace(" " , "_"));
+           // or
+       name = name.replace(" ","-");
+        System.out.println(name);
+}}
+    
+

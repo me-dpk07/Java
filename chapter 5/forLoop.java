909a542ae2df73912afc27e77e92044d21bd41ba
Deepak <deepakprasad9554.com>
2024-07-13 16:05:15 +0530

    printed n odd numbers using for loop loop

--- chapter 5/forLoop.java
+++ chapter 5/forLoop.java
@@ -1,7 +1,8 @@
 public class forLoop {
     public static void main(String[] args) {
+        int n = 1;
         for(int i=1; i<=100; i++){
-        System.out.println(i);
+        System.out.println(2*i+1);
         }
     }
     

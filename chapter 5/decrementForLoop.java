ec31b8106536c46b11fa9cc1c10007e925fa9f06
Deepak <deepakprasad9554.com>
2024-07-13 16:12:42 +0530

    decrement For loop

---
+++ chapter 5/decrementForLoop.java
@@ -0,0 +1,11 @@
+public class decrementForLoop {
+    public static void main(String[] args) {
+        int n = 0;
+        for(int i= 15; i>n; i--){
+            System.out.println(i);
+        }
+        
+        
+    }
+   
+}

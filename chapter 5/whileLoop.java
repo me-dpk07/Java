6cc72330df8f264156a3ce3c18c28f532a13b94c
Deepak <deepakprasad9554.com>
2024-07-13 15:37:31 +0530

    printing 100 - 200 natural number

---
+++ chapter 5/whileLoop.java
@@ -0,0 +1,12 @@
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 100;
+        while(i>=100 && i<=200){
+            System.out.println(i);
+            i++;
+
+        }
+    }
+    
+}

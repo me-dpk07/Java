ce1d3aec8bb910a1478110b0c8e00d4b3203e90c
Deepak <deepakprasad9554.com>
2024-07-13 15:49:56 +0530

    printed n natural numbers using do while loop

---
+++ chapter 5/dowhileloop.java
@@ -0,0 +1,12 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        int a = 10;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<=100);
+    }
+    
+}
+
+//in do while loop first loops is printed and then condition will be checked

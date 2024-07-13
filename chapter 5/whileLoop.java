91277ef59543b492841f3f255452630a7530b3dc
Deepak <deepakprasad9554.com>
2024-07-13 15:39:50 +0530

    printing 100 - 200 nbatural numbers

--- chapter 5/whileLoop.java
+++ chapter 5/whileLoop.java
@@ -2,7 +2,7 @@
 public class whileLoop {
     public static void main(String[] args) {
         int i = 100;
-        while(i>=100 && i<=200){
+        while(i<=200){
             System.out.println(i);
             i++;
 

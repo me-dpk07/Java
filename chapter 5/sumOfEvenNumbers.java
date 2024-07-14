6fa6725cbbb321017a947013267da3bf2fa1977c
Deepak <deepakprasad9554.com>
2024-07-14 15:53:19 +0530

    sum of n even numbers

---
+++ chapter 5/sumOfEvenNumbers.java
@@ -0,0 +1,11 @@
+public class sumOfEvenNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+     int n = 100;
+     for(int i = 0; i<100; i++){
+     System.out.println(2*i);
+     sum = sum + (2*i);}
+     System.out.println(sum);
+    }
+}
+

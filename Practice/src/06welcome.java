aad9ce5bd027e6459e4f04aa7e9fb3a35a972a6c
Deepak <deepakprasad9554.com>
2024-07-17 21:13:34 +0530

    welcome

--- Practice/src/06welcome.java
+++ Practice/src/06welcome.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class welcome {
+class welcome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("enter your name");

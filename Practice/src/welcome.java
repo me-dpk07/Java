c12cf9c39a17e2f7b0754880c15c2e97c49dda67
Deepak <deepakprasad9554.com>
2024-07-17 16:53:47 +0530

    welcome java

---
+++ Practice/src/welcome.java
@@ -0,0 +1,7 @@
+public class welcome {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n*****\n");
+        System.out.println("*****\n****\n***\n**\n*");
+        System.out.println("    *\n   **\n  ***\n ****\n*****");
+    }
+}

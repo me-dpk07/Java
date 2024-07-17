d100a4bacda4ae839b65d74df25750ffa9797694
Deepak <deepakprasad9554.com>
2024-07-17 16:53:09 +0530

    takimg user input

---
+++ 01 Datatypes variables and inputs/src/userinput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class userinput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String name = input.nextLine();
+        System.out.println("gud morning "+name+"!! " );
+        System.out.println(name +"also tell me your age");
+        String age = input.nextLine();
+        System.out.println("hey "+ name + " your age is "+ age);
+
+    }
+}

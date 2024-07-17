f0911a4b722fdfb678a5b7a63ee7d827bb0a7e65
Deepak <deepakprasad9554.com>
2024-07-17 22:20:24 +0530

    arthmetic problem taking inputs by user

--- Practice/src/08Arthmetic.java
+++ Practice/src/08Arthmetic.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Arthmetic {
+class Arthmetic {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("enter your first number");

c888f6825fb0e86b6566a3b1991dc89cb0ee1db0
Deepak <deepakprasad9554.com>
2024-07-17 17:11:38 +0530

    Merge remote-tracking branch 'origin/java' into java

--- Practice/src/welcome.java
+++
@@ -1,7 +0,0 @@
-public class welcome {
-    public static void main(String[] args) {
-        System.out.println("*\n**\n***\n****\n*****\n");
-        System.out.println("*****\n****\n***\n**\n*");
-        System.out.println("    *\n   **\n  ***\n ****\n*****");
-    }
-}

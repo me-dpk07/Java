4ccb061b07da1ab4683e06e856fda868058f5002
Deepak <deepakprasad9554.com>
2024-07-18 08:51:48 +0530

    multiplication of two float numbers

--- Practice/src/09FloatMultiplication.java
+++ Practice/src/09FloatMultiplication.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class FloatMultiplication {
+class FloatMultiplication {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your first number ");

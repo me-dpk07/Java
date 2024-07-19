486842bac32fa537207df963c41ed5285b7f9018
Deepak <deepakprasad9554.com>
2024-07-19 20:12:00 +0530

    table of given number

---
+++ Practice/src/MultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+        table(num);
+    }
+    public static void table(int num){
+        int i = 1;
+        while(i <= 10){
+            System.out.println(num + " X " + i + " = " +(num*i) );
+            i++;
+        }
+    }
+}

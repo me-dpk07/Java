aada0407fcbcdb5d3b8c21ef6924944882baa597
Deepak <deepakprasad9554.com>
2024-07-19 20:58:13 +0530

    table making of given number

--- Practice/src/27MultiplicationTable.java
+++ Practice/src/27MultiplicationTable.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class MultiplicationTable {
+class MultiplicationTable {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number");

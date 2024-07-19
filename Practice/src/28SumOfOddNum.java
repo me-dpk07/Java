a5182b76fd40ef6a1ce0316271c47b397214bd95
Deepak <deepakprasad9554.com>
2024-07-19 20:57:57 +0530

    sum of the given odd number

--- Practice/src/28SumOfOddNum.java
+++ Practice/src/28SumOfOddNum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SumOfOddNum {
+class SumOfOddNum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the value of N");
